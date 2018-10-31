460678264f1ed51a6f4cc9e4fb1bef1bf423af2b
97joaquinhr <97joaquinhr@gmail.com>
2018-10-31 08:34:58 -0600

    exam review and p9 euler

---
+++ Workshops/p9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(){
+	int a,b,c;
+	for (c=1000;c>3;c--)
+	{
+		for(b=c-1;b>2;b--)
+		{
+			for(a=b-1;a>1;a--)
+			{
+				if(a+b+c==1000){
+					if(a*a+b*b==c*c){
+						printf("a=%i,b=%i,c=%i",a,b,c);
+						return 0;
+					}
+				}
+			}
+		}
+	}
+}
